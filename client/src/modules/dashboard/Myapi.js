1f803a1d2836e4089c17ad25fc4804a16a928687
gauravthakur2121 <shishodiag12@gmail.com>
2024-11-06 19:07:58 +0530

    changes

--- client/src/modules/dashboard/Myapi.js
+++ client/src/modules/dashboard/Myapi.js
@@ -1,5 +1,5 @@
 
 
-    // const Myapi = "http://localhost:8900";
-        const Myapi = "https://ranagaurav.onrender.com"
+    //  const Myapi = "http://localhost:8900";
+      const Myapi = "https://ranagaurav.onrender.com"
 export default Myapi;
